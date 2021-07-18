8d11f3ccee404fd4413dcc4f54668eb02df2e54a
Elena <e.mandrovska@gmail.com>
2021-07-19 00:11:36 +0300

    Updates

--- static/js/main.604582d9.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-02-phonebook"]=this["webpackJsonpgoit-react-hw-02-phonebook"]||[]).push([[0],{10:function(t,e,n){"use strict";n.r(e);var o=n(1),c=n.n(o),r=n(3),s=n.n(r),i=(n(8),n(0));var a=function(){return Object(i.jsx)("div",{className:"App"})};s.a.render(Object(i.jsx)(c.a.StrictMode,{children:Object(i.jsx)(a,{})}),document.getElementById("root"))},8:function(t,e,n){}},[[10,1,2]]]);
-//# sourceMappingURL=main.604582d9.chunk.js.map
